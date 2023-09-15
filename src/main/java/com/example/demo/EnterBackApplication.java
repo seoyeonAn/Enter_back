3e3a2337cea1dd7b40147aeae13c8e913745c479
tion <seoyson31@gmail.com>
2023-09-15 18:11:38 +0900

    230915

---
+++ src/main/java/com/example/demo/EnterBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterBackApplication.class, args);
+	}
+
+}
