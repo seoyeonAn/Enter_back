4caee01a627ba2e0c3cdd4a5a920e8d50bbd6168
yuhyelim <hhllsummer07@gmail.com>
2023-09-26 01:19:53 +0900

    230926 hye 01:19

--- src/main/java/com/example/demo/enter/controller/EnterController.java
+++ src/main/java/com/example/demo/enter/controller/EnterController.java
@@ -1,16 +1,12 @@
 package com.example.demo.enter.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,11 +22,6 @@ public class EnterController {
 	public EnterController() {}
 	
 	// http://localhost:8090/enter
-//	@GetMapping("/enter")
-//	public List<EnterDTO> getList() throws Exception{
-//		return enterService.enterList();
-//	}
-	
 	@GetMapping("/enter")
 	public Map<String, Object> getList(){
 		Map<String, Object> map = new HashMap<>();
@@ -38,11 +29,15 @@ public class EnterController {
 		return map;
 	}
 	
-	@PostMapping("/enter/insert")
+//	@PostMapping("/enter/insert")
+//	public void insertExcute(@RequestBody EnterDTO dto) {	
+//		enterService.insert(dto);
+//	} 
+
+	@PostMapping("/enter")
 	public void insertExcute(@RequestBody EnterDTO dto) {	
 		enterService.insert(dto);
 	} 
-	
 	// http://localhost:8090/enter/1/0
 //	@PutMapping("/enter/{enter_seq}/{completed}")
 //	public ResponseEntity<Object> putEnter(@PathVariable("enter_seq") long enter_seq, @PathVariable("completed") long completed) throws Exception {
