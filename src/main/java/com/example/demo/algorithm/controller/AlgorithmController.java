9c9f517a0ea3e398c98f5286095c2b7468da2557
yuhyelim <hhllsummer07@gmail.com>
2023-10-17 19:05:14 +0900

    231017 hye 19:05

--- src/main/java/com/example/demo/algorithm/controller/AlgorithmController.java
+++ src/main/java/com/example/demo/algorithm/controller/AlgorithmController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
 //http://127.0.0.1:5000
 @CrossOrigin("*")
 @RestController
 public class AlgorithmController {	
 	@GetMapping("/")
     public String callFlaskFunction() {
-		 // Flask 함수 호출을 위한 코드
 		String flaskFunctionUrl = "http://127.0.0.1:5000";
         ResponseEntity<String> response = new RestTemplate().getForEntity(flaskFunctionUrl, String.class);
         String result = response.getBody();
